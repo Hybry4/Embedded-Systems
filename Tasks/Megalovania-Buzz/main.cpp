012611840d9ce9bfa08279ab28f1d3e23d3b6565
Hybry4 <146357117+Hybry4@users.noreply.github.com>
2023-10-05 18:24:40 +0100

    Updated Megalovania
    V1.1

--- Tasks/Megalovania-Buzz/main.cpp
+++ Tasks/Megalovania-Buzz/main.cpp
@@ -14,47 +14,187 @@ Buttons buttons;
 
 int main()
 {
+    volatile int SongBar = 1;
     //Wait for the BLUE button to be pressed (otherwise this becomes super annoying!)
     while (buttons.BlueButton == 0);
     
     //Repeat everything "forever" (until the power is removed or the chip is reset)
     while (true)
-    {
-     {//D
+    { 
+        printf("Bar: %d\n", SongBar);
+     if         (SongBar == 1) 
+     {
+      {//D
         redLED = 1;
         buzz.playTone("D");
-        wait_us(WAIT_TIME_MS * 100);  //100ms
+        wait_us(WAIT_TIME_MS * 60);
 
         redLED = 0;
         buzz.rest();
-        wait_us(WAIT_TIME_MS * 100);  //100ms
+        wait_us(WAIT_TIME_MS * 65);
         }
-     {//D
+      {//D
         redLED = 1;
         buzz.playTone("D");
-        wait_us(WAIT_TIME_MS * 100);  //100ms
+        wait_us(WAIT_TIME_MS * 60);
 
         redLED = 0;
         buzz.rest();
-        wait_us(WAIT_TIME_MS * 100);  //100ms
+        wait_us(WAIT_TIME_MS * 65);
         }
+     }
+     else if    (SongBar == 2) 
+     {
+      {//C
+        redLED = 1;
+        buzz.playTone("C");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+      {//C
+        redLED = 1;
+        buzz.playTone("C");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+     }
+     else if    (SongBar == 3) 
+     {
+      {//B Lower
+        redLED = 1;
+        buzz.playTone("B");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+      {//B Lower
+        redLED = 1;
+        buzz.playTone("B");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+     }
+     else if    (SongBar == 4) 
+     {
+      {//A# Lower
+        redLED = 1;
+        buzz.playTone("A#");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+      {//A# Lower
+        redLED = 1;
+        buzz.playTone("A#");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+    // revert SongBar to repeat the cycle
+     SongBar = 0;
+     }
      {//D Higher
         redLED = 1;
         buzz.playTone("D", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 100);  //100ms
+        wait_us(WAIT_TIME_MS * 60);
 
         redLED = 0;
         buzz.rest();
-        wait_us(WAIT_TIME_MS * 300);  //300ms
+        wait_us(WAIT_TIME_MS * 65);
         }
+     {//Rest
+      wait_us(WAIT_TIME_MS * 125);
+      }
      {//A
         redLED = 1;
         buzz.playTone("A", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 100);  //100ms
+        wait_us(WAIT_TIME_MS * 60);
 
         redLED = 0;
         buzz.rest();
-        wait_us(WAIT_TIME_MS * 1000);  //1000ms
+        wait_us(WAIT_TIME_MS * 65);
         }
+     {//Rest
+      wait_us(WAIT_TIME_MS * 125);
+      }
+     {//Rest
+      wait_us(WAIT_TIME_MS * 125);
+      }
+     {//G#
+        redLED = 1;
+        buzz.playTone("G#");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+     {//Rest
+      wait_us(WAIT_TIME_MS * 125);
+      }
+     {//G
+        redLED = 1;
+        buzz.playTone("G");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+     {//Rest
+      wait_us(WAIT_TIME_MS * 125);
+      }
+     {//F Quarter
+        redLED = 1;
+        buzz.playTone("F");
+        wait_us(WAIT_TIME_MS * 200);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 50);
+        }
+     {//D
+        redLED = 1;
+        buzz.playTone("D");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+     {//F
+        redLED = 1;
+        buzz.playTone("F");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+     {//G
+        redLED = 1;
+        buzz.playTone("G");
+        wait_us(WAIT_TIME_MS * 60);
+
+        redLED = 0;
+        buzz.rest();
+        wait_us(WAIT_TIME_MS * 65);
+        }
+     SongBar = SongBar + 1;
     }
 }
